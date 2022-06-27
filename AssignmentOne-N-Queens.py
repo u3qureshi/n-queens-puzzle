7fa8001857b1686756e9eb29698c78b2a80711f4
Usman <usman-qureshi@outlook.com>
2022-06-27 18:23:48 -0400

    Printing all possible solutions to the n-queens puzzle for each given n

--- AssignmentOne-N-Queens.py
+++ AssignmentOne-N-Queens.py
@@ -1,72 +1,80 @@
 # START OF CLASS ----------------
+totalSolutions = 0
+
 class Puzzle:
 
     def __init__(self,N):
         self.N = N
-        self.BOARD = [[0 for i in range(self.N)] for j in range(self.N)]
     
-    def displayBoard(self):
+    def displayBoard(self, BOARD):
+        print('------------')
         for i in range(self.N):
             for j in range(self.N):
-                print(self.BOARD[i][j], end=' ')
+                print(BOARD[i][j], end=' ')
             print('')
 
     # Method to check if a position is safe to place a queen
-    def isSafe(self, row, column):
+    def isSafe(self, BOARD, row, column):
 
         # Check the column of the queen
         for i in range(self.N):
-            if (self.BOARD[i][column] == 1):
+            if (BOARD[i][column] == 1):
                 return False
 
         # Check the left side of the row
         for j in range(self.N):
-            if (self.BOARD[row][j] == 1):
+            if (BOARD[row][j] == 1):
                 return False
 
         # Check the left side upper diagonal
         for i, j in zip(range(row, -1, -1), range(column, -1, -1)):
-            if (self.BOARD[i][j] == 1):
+            if (BOARD[i][j] == 1):
                 return False
 
         # Check the left side lower diagonal
         for i, j in zip(range(row, self.N, 1), range(column, -1, -1)):
-            if (self.BOARD[i][j] == 1):
+            if (BOARD[i][j] == 1):
                 return False
         
         # Check the right side upper diagonal
         for i, j in zip(range(row, -1, -1), range(column, self.N, 1)):
-            if (self.BOARD[i][j] == 1):
+            if (BOARD[i][j] == 1):
                 return False
 
         # Check the right side lower diagonal
         for i, j in zip(range(row, self.N, 1), range(column, self.N, 1)):
-            if (self.BOARD[i][j] == 1):
+            if (BOARD[i][j] == 1):
                 return False
 
         # Return true if all tests are passed and queen can safely be placed
         return True
 
-    def placeQueen(self, queenCount):
+    def placeQueen(self,BOARD, row):
+        
+        global totalSolutions
 
         # Base case
-        if (queenCount == self.N):
+        if (row == self.N):
+            self.displayBoard(BOARD)
+            totalSolutions += 1
             return True
         
-        for row in range(self.N):
-            for col in range(self.N):
-                if (self.isSafe(row, col)):
-                    self.BOARD[row][col] = 1
-                    queenCount += 1
-                    if (self.placeQueen(queenCount) == True):
-                        return True
-                    else:
-                        self.BOARD[row][col] = 0
-                        queenCount -= 1
+        for c in range(self.N):
+            if (self.isSafe(BOARD, row, c)):
+                # If the queen is safe to place without attack, then place queen at current location
+                BOARD[row][c] = 1
+                    
+                # Recurse to the next row ++
+                self.placeQueen(BOARD, row + 1)
+
+                # Backtrack and undo queen placement
+                BOARD[row][c] = 0
         return False
 
 # END OF CLASS ----------------
 
-puzzle = Puzzle(int(input("Enter the value of N: ")))
-puzzle.placeQueen(0)
-puzzle.displayBoard()
+# MAIN SECTION
+N = int(input("Enter the value of N: "))
+BOARD = [[0 for i in range(N)] for j in range(N)]
+Puzzle(N).placeQueen(BOARD, 0)
+print(f'There exists {totalSolutions} total solutions to the {N}-Queens puzzle.')
